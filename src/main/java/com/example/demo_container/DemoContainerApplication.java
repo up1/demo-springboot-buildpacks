8bf4ba621ca254d8f4ade40992ab54702db53e0f
up1 <somkiat.p@gmail.com>
2020-01-28 13:45:41 +0700

    Demo

---
+++ src/main/java/com/example/demo_container/DemoContainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoContainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_container/DemoContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_container;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
